ac4ebb7ed197fc4d97452756b5719a63cd8a6a44
b4158813 <814295903@qq.com>
2023-04-10 22:24:39 +0800

    修改ServerHost

--- log/config.go
+++ log/config.go
@@ -1,7 +1,7 @@
 package log
 
 const (
-	ServerHost   = "localhost"
+	ServerHost   = "127.0.0.1"
 	ServerPort   = ":4000"
 	ServerRouter = "/log"
 	ServiceUrl   = "http://" + ServerHost + ServerPort + ServerRouter
--- registry/config.go
+++ registry/config.go
@@ -1,7 +1,7 @@
 package registry
 
 const (
-	ServerHost   = "localhost"
+	ServerHost   = "127.0.0.1"
 	ServerPort   = ":3000"
 	ServerRouter = "/services"
 	ServiceUrl   = "http://" + ServerHost + ServerPort + ServerRouter
